0ac1edc0b7dcecd88f8ac563c9a7cd0d95c1a1e5
maxey0810 <maxey0810@gmail.com>
2025-09-15 16:57:56 +0800

    Add files via upload

---
+++ PAGF_fusion/PAGF_run.m
@@ -0,0 +1,32 @@
+close all;clc;clear;
+addpath metrics\
+
+% I\O setting
+input_path = 'input/';
+
+
+% parameters
+img_name = 'Camp1827.jpg'; 
+IR = im2double(imread(strcat(input_path,'IR/', img_name)));
+
+VI = im2double(imread(strcat(input_path,'VI/', img_name)));
+
+
+   F=PAGF_fusion(IR,VI);
+
+
+figure;imshow([IR,VI,F]);title('PAGF');
+
+
+F=im2uint8(F);
+IR=im2uint8(IR);
+VI=im2uint8(VI);
+
+PAGF_CE = metricsCross_entropy(IR,VI,F);
+PAGF_AG = metricsAvg_gradient(IR,VI,F);
+PAGF_SF = metricsSpatial_frequency(IR,VI,F);
+PAGF_SCD=metricsScd(IR,VI,F);
+PAGF_EI = metricsEdge_intensity(IR,VI,F);
+PAGF_VIF = metricsVif(IR,VI,F);
+
+
