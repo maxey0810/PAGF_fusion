2148eebd714d6baaa76fe2446caab61165f3f94e
maxey0810 <maxey0810@gmail.com>
2025-09-22 17:46:25 +0800

    Update PAGF_run.m

--- PAGF_fusion/PAGF_run.m
+++ PAGF_fusion/PAGF_run.m
@@ -4,15 +4,16 @@ addpath metrics\
 % I\O setting
 input_path = 'input/';
 
-% parameters
 img_name = 'Camp1827.jpg'; 
 IR = im2double(imread(strcat(input_path,'IR/', img_name)));
 
 VI = im2double(imread(strcat(input_path,'VI/', img_name)));
 
 % If the energy information in visible images becomes unreliable under extreme conditions, 
-% it is recommended to manually set a large nonlinear weight (e.g., >20 in 'meting.jpg').
-   F=PAGF_fusion(IR,VI);
+% it is recommended to manually set a large nonlinear weight parameter (e.g., >20 in 'meting.jpg').
+
+   F=PAGF_fusion(IR,VI); %adaptive parameters
+   %F=PAGF_fusion(IR,VI,b_fixed);%manually set a large nonlinear weight parameter
 
 figure;imshow([IR,VI,F]);title('PAGF');
 
@@ -27,6 +28,7 @@ PAGF_SF = metricsSpatial_frequency(IR,VI,F);
 PAGF_SCD=metricsScd(IR,VI,F);
 PAGF_EI = metricsEdge_intensity(IR,VI,F);
 PAGF_VIF = metricsVif(IR,VI,F);
+
 
 
 
